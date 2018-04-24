802d2adaca05718c470b560eabe61023394638e5
Prashant Kumar Tiwari <kumartiwariprashant@gmail.com>
2018-04-25 04:12:10 +0530

    SAD Project

--- app/src/main/java/com/prashant/root/be/blood_banks.java
+++ app/src/main/java/com/prashant/root/be/blood_banks.java
@@ -1,13 +1,28 @@
 package com.prashant.root.be;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class blood_banks extends AppCompatActivity {
+    private ImageView location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blood_banks);
+
+        location =findViewById(R.id.location);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/dir/26.2496953,78.1698474/dd+mall+location+maps/@26.232404,78.1441663,13z/data=!3m2!4b1!5s0x3976c42842f59eaf:0xd6a1f03027683535!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3976c42868348417:0xed839ed3252040!2m2!1d78.1663396!2d26.2093128")));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/prashant/root/be/donation_form.java
+++ app/src/main/java/com/prashant/root/be/donation_form.java
@@ -1,7 +1,7 @@
 package com.prashant.root.be;
 
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,11 +12,12 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+
 public class donation_form extends AppCompatActivity {
 
     private  Button button;
-    private EditText uid;
     private EditText fullName;
+    private TextInputEditText uid_aadhar;
     private EditText units;
 
     @Override
@@ -24,7 +25,7 @@ public class donation_form extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_form);
 
-        uid = findViewById(R.id.input_uid);
+        uid_aadhar = findViewById(R.id.input_uid);
         fullName = findViewById(R.id.input_name);
         units = findViewById(R.id.input_units);
         button = findViewById(R.id.button_done_end);
@@ -32,8 +33,9 @@ public class donation_form extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 {
-                    if (TextUtils.isEmpty(uid.getText())){
-                        Toast.makeText(donation_form.this, "UId/Aadhar Number Can't be null!", Toast.LENGTH_SHORT).show();
+                    if (uid_aadhar.length()!=12 ){
+
+                        Toast.makeText(donation_form.this, "UId/Aadhar Number invalid", Toast.LENGTH_SHORT).show();
                     }
                     else if (TextUtils.isEmpty(fullName.getText())){
                         Toast.makeText(donation_form.this, "Invalid Name", Toast.LENGTH_SHORT).show();
@@ -44,7 +46,7 @@ public class donation_form extends AppCompatActivity {
                     else
                     {
                         Toast.makeText(donation_form.this, "Recorded", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(donation_form.this,blood_banks.class));
+                        startActivity(new Intent(donation_form.this,get_blood.class));
                     }
 
                 }
@@ -55,42 +57,15 @@ public class donation_form extends AppCompatActivity {
 
 
 
-        Spinner spinner3 = (Spinner) findViewById(R.id.spinner_blood_group);
+        Spinner spinner3 =findViewById(R.id.gender);
         ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
-                R.array.blood_group, android.R.layout.simple_spinner_item);
+                R.array.gender, android.R.layout.simple_spinner_item);
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner3.setAdapter(adapter3);
 
 
 
 
-/*
-
-        public void onClickCheck(){
-
-            String uid2 = uid.getText().toString();
-            String name2 = fullName.getText().toString();
-            String units2 = units.getText().toString();
-
-            if (TextUtils.isEmpty(uid.getText()))
-
-                if (uid2.equals("")){
-                    Toast.makeText(this, "UId/Aadhar Number Can't be null!", Toast.LENGTH_SHORT).show();
-                }
-
-            if (name2.equals("")){
-                Toast.makeText(this, "Invalid Name", Toast.LENGTH_SHORT).show();
-            }
-
-            if (units2.equals("")){
-                Toast.makeText(this, "Invalid units of Blood Donation", Toast.LENGTH_SHORT).show();
-            }
-
-
-        }
-        */
-
     }
-
 
 }
